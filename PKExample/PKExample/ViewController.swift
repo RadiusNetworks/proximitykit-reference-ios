bc6b16b4608e8c2d42c2efdf61fc732379698b9e
Scott Newman <snewman18@gmail.com>
2016-05-11 12:57:57 -0600

    Updated app for Swift 2 and ProximityKit 1.2.7

---
+++ PKExample/PKExample/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  PKExample
+//
+//  Created by Scott Newman on 5/11/16.
+//  Copyright © 2016 Radius Networks. All rights reserved.
+//
+
+import UIKit
+import ProximityKit
+
+class ViewController: UIViewController, RPKManagerDelegate {
+
+  var proximityKitManager: RPKManager?
+  
+  override func viewDidLoad() {
+    
+    super.viewDidLoad()
+
+    if let path = NSBundle.mainBundle().pathForResource("ProximityKit", ofType:"plist") {
+      if let dict = NSDictionary(contentsOfFile:path) {
+        self.proximityKitManager = RPKManager(delegate:self, andConfig:dict as [NSObject : AnyObject])
+      }
+    }
+    else {
+      let reason = "The ProximityKit.plist configuration file is missing."
+      NSException(name:"Missing Configuration File", reason:reason, userInfo:nil).raise()
+    }
+    
+    if let proximityKitManager = self.proximityKitManager {
+      proximityKitManager.start()
+    }
+    
+  }
+
+  // MARK: Proximity Kit Delegate Methods
+  
+  func proximityKitDidSync(manager : RPKManager) {
+    print("Proximity Kit did sync")
+  }
+  
+  func proximityKit(manager: RPKManager!, didDetermineState state: RPKRegionState, forRegion region: RPKRegion!) {
+    
+    if state == .Inside {
+        print("State Changed: Inside Region \(region.name) (\(region.identifier))")
+    }
+    else if state == .Outside {
+      print("State Changed: Outside Region \(region.name) (\(region.identifier))")
+    }
+    else if state == .Unknown {
+      print("State Changed: Unknown Region \(region.name) (\(region.identifier))")
+    }
+    
+  }
+  
+  func proximityKit(manager : RPKManager, didEnter region:RPKRegion) {
+    print("Entered Region \(region.name), \(region.identifier)");
+  }
+  
+  func proximityKit(manager : RPKManager, didExit region:RPKRegion) {
+    print("Exited Region \(region.name), \(region.identifier)");
+  }
+  
+  func proximityKit(manager: RPKManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: RPKBeaconRegion!) {
+    for beacon in beacons as! [RPKBeacon] {
+      print("Major: \(beacon.major), Minor: \(beacon.minor)")
+    }
+  }
+
+}
+
